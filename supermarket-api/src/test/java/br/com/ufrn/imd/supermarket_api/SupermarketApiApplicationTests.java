b0c7f3ab7af705c8b2d0aec614b6b717bc8f5336
elsonfreire <elsonfreirefilho@gmail.com>
2024-10-23 19:32:55 -0300

    :rocket: Primeiro commit

---
+++ supermarket-api/src/test/java/br/com/ufrn/imd/supermarket_api/SupermarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufrn.imd.supermarket_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
