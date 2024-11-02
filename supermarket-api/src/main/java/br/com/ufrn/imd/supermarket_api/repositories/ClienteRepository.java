f296d8c6c0b0847b5bb520c45861519429c7c740
elsonfreire <elsonfreirefilho@gmail.com>
2024-11-02 11:00:32 -0300

    ajustes

--- supermarket-api/src/main/java/br/com/ufrn/imd/supermarket_api/repositories/ClienteRepository.java
+++ supermarket-api/src/main/java/br/com/ufrn/imd/supermarket_api/repositories/ClienteRepository.java
@@ -2,6 +2,8 @@ package br.com.ufrn.imd.supermarket_api.repositories;
 
 import br.com.ufrn.imd.supermarket_api.model.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
 }
